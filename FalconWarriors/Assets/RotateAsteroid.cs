da3b155ff97035875ba329b291890aa33b353e7b
paraleul <tbi.loading@gmail.com>
2017-01-04 14:39:16 +0200

    Added rotating text
    Rotating text (all levels for now)

--- FalconWarriors/Assets/RotateAsteroid.cs
+++ FalconWarriors/Assets/RotateAsteroid.cs
@@ -4,12 +4,17 @@ using System.Collections;
 public class RotateAsteroid : MonoBehaviour {
 
     public float tumble;
+    public bool onlyHorizontal;
 
     private Vector3 angVelo;
 
 	// Use this for initialization
 	void Start () {
         angVelo = Random.insideUnitSphere * tumble;
+        if (onlyHorizontal)
+        {
+            angVelo = new Vector3(0, tumble, 0);
+        }
 	}
 	
 	// Update is called once per frame
